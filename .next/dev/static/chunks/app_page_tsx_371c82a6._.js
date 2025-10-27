56c25b63ef270b4b5ba0f47d91adeb3550cdb6e3
Khawar Hanif Khokhar <88875448+KhawarKhokhar@users.noreply.github.com>
2025-10-27 22:27:55 +0500

    Your descriptive commit message

---
+++ .next/dev/static/chunks/app_page_tsx_371c82a6._.js
@@ -0,0 +1,7 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/app_components_Home_HeroSection_tsx_ad629dfe._.js"
+],
+    source: "dynamic"
+});
