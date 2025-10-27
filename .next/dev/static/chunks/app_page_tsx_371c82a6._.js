f528458705983d7b571bb2cb918fd95e147fa6a0
Khawar Hanif Khokhar <88875448+KhawarKhokhar@users.noreply.github.com>
2025-10-27 22:23:59 +0500

    Clean repo and add .gitignore

--- .next/dev/static/chunks/app_page_tsx_371c82a6._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_components_Home_HeroSection_tsx_ad629dfe._.js"
-],
-    source: "dynamic"
-});
