58c464bade2cac0c44c0283bd58c29a75ac1ae45
Khawar Hanif Khokhar <88875448+KhawarKhokhar@users.noreply.github.com>
2025-10-31 23:45:45 +0500

    Delete .next directory

--- .next/dev/static/chunks/app_page_tsx_67561f62._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_32435b26._.js"
-],
-    source: "dynamic"
-});
